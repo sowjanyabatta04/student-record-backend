d6a0f8c8d1df2c2ef2a0fec551a056710c9d92cf
sowjanyabatta04 <sowjanyabatta04@gmail.com>
2025-10-23 01:51:17 +0530

    Fix catch-all React route for Render deployment

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.delete("/api/students/:rollNumber", async (req, res) => {
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("/*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
   });
 }
